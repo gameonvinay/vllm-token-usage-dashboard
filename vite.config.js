72e981d4192339e7780ba5473f3bf8a14e9df4b6
gameonvinay <nfstarzan@gmail.com>
2026-08-16 00:45:46 +0530

    Enable polling in Vite watcher to bypass Linux inotify ENOSPC limit

--- vite.config.js
+++ vite.config.js
@@ -50,6 +50,8 @@ export default defineConfig({
   },
   server: {
     watch: {
+      usePolling: true,
+      interval: 1000,
       ignored: ['**/node_modules/**', '**/.git/**', '**/dist/**'],
     },
     proxy: {
